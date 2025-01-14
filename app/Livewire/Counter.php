d89279848fec01334d6dc0c1161bc69702afa682
Alessio_Grieco <Alessiogrieco218@gmail.com>
2025-01-14 17:46:14 +0100

    Metodi increment e decrement completati

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $value = 0;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function increment()
+    {
+        $this->value++;
+    }
+
+    public function decrement()
+    {
+        $this->value--;
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Laravel Livewire</h1>
+            </div>
+        </div>
+    </div>
+    <livewire:counter />
+</x-layout>
